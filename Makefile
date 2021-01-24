f30ed082cd53ce2b03ab3ee70787cfe5cd60203e
Laurel <laurel.o.anderson@wsu.edu>
2021-01-24 14:15:05 -0800

    I have the basic argument stucture figured out.

--- Makefile
+++ Makefile
@@ -4,8 +4,14 @@ output:
 clean:
 	rm output
 
-run: output
+test1: output
 	./output text.txt
+	
+test2: output
+	./output text.txt text2.txt text3.txt
+	
+test3: output
+	./output 45 text.txt text2.txt text3.txt
 	
 test: 
 	valgrind --leak-check=yes ./output text.txt
--- main.c
+++ main.c
@@ -6,13 +6,47 @@
 int main( int argc, char *argv[] )  {
 
 
-	// ERROR: cheak if there are the approprate amount of args
-	if (argc < 2) {
+	// cheak if there are the approprate amount of args
+	if (argc < 2){
 	
 		fprintf(stderr, "ERROR: You need to pass at least 1 file to the program\n"); 
 		return 1; 
+		
 	}
+	
+	int start;
+	char *ptr = argv[1]; 
+	int num = strtol(ptr, &ptr, 10); 
+	
+	// check if the first argument is a num
+	if (*ptr == '\0'){
+	 	
+	 	//check to make sure you don't only have a number. 
+		if (argc == 2){
+			fprintf(stderr, "ERROR: specify a file to parse.\n"); 
+			return 1; 
+		}
+		
+		// set argument start point to 2
+		start = 2; 
+		printf("First argument is number %d and second is %s\n", num, argv[2]); 
+		
+	}else{ 
+	
+		// set argument start point to 1
+		start = 1; 
+		printf("First argument is not a number\n"); 
+		
+	}
+	
+	// Iterate through all text files passed as arguments
+	for (int i = start; i < argc; i++){
+		
+		printf("Parsing file %s...\n", argv[i]);
 
+	}
+	
+	/*
 	char *str; 
 	FILE *fp = fopen(argv[1], "r");
 	
@@ -21,6 +55,7 @@ int main( int argc, char *argv[] )  {
 	
 	free(str); 
 	fclose(fp);
+	*/
 	
 	return 0; 
 	
